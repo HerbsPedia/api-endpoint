f21ef678aa19023836c7203c264c3aa3476b4761
daniizzulhaq <43689314+daniizzulhaq@users.noreply.github.com>
2024-06-09 23:24:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/api/plants', (req, res) => {
     res.json(plantsData);
 });
 
+// Endpoint untuk menampilkan satu jenis tanaman berdasarkan ID
+app.get('/api/plants/:id', (req, res) => {
+    const plantId = req.params.id;
+    const plant = plantsData.find(plant => plant.id === parseInt(plantId));
+    if (!plant) {
+        res.status(404).json({ error: 'Tanaman tidak ditemukan' });
+    } else {
+        res.json(plant);
+    }
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server berjalan pada port ${PORT}`);
